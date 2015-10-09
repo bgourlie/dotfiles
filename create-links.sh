2f83cc884e693abbe3d29a5fbd101494b002f5ab
W. Brian Gourlie <bgourlie@gmail.com>
2015-10-09 11:05:46 -0500

    Add basic sublime text vintageous mappings

--- create-links.sh
+++ create-links.sh
@@ -6,6 +6,7 @@ ln -s $DOTFILES/.tmux.conf ~/.tmux.conf
 ln -s $DOTFILES/.vimrc ~/.vimrc
 ln -s $DOTFILES/.vimrc ~/.nvimrc
 ln -s $DOTFILES/.ideavimrc ~/.ideavimrc
+ln -s $DOTFILES/Default.sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default.sublime-keymap
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
