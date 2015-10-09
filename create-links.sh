d73f5c9c58f90e3e3a30d6b6dbcb7f6ee7051216
W. Brian Gourlie <bgourlie@gmail.com>
2015-10-09 09:36:06 -0500

    Add .ideavimrc, which sources .vimrc

--- create-links.sh
+++ create-links.sh
@@ -5,9 +5,9 @@ DOTFILES=$(pwd)
 ln -s $DOTFILES/.tmux.conf ~/.tmux.conf
 ln -s $DOTFILES/.vimrc ~/.vimrc
 ln -s $DOTFILES/.vimrc ~/.nvimrc
+ln -s $DOTFILES/.ideavimrc ~/.ideavimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 
 ln -s $DOTFILES/.vim/autoload ~/.nvim
